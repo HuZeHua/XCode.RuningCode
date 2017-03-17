492224d71e41a584f65c7c24f00c2213eede91ba
胡泽华 <420681321@qq.com>
2017-03-17 15:32:15 +0800

    commit

---
+++ XCode.RuningCode/XCode.RuningCode.Web/XCodeScriptBundle.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Optimization;
+
+namespace XCode.RuningCode.Web
+{
+    public class XCodeScriptBundle : ScriptBundle
+    {
+        readonly JavascriptObfuscator _jso = new JavascriptObfuscator();
+
+        public XCodeScriptBundle(string virtrualPath)
+            : base(virtrualPath)
+        {
+            Transforms.Add(_jso);
+        }
+    }
+}
