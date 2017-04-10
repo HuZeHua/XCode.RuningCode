6396dacee4d0ec0eb548f98bc3d9d13ddba84a72
胡泽华 <420681321@qq.com>
2017-04-10 15:06:46 +0800

    modify

---
+++ XCode.RuningCode/XCode.RuningCode.Web/Controllers/BlogController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace XCode.RuningCode.Web.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Detial()
+        {
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            return View();
+        }
+    }
+}
